3dcc5fdf4c999ac80b9705578a5b58f7663c3218
Muhammad Yusuf <musufyay@Gmail.com>
2017-01-10 11:41:42 -0500

    Create walkingGait.groovy

---
+++ WalkingEngine/walkingGait.groovy
@@ -0,0 +1,35 @@
+import java.time.Duration;
+
+import java.util.ArrayList;
+
+import javafx.application.Platform;
+
+import org.reactfx.util.FxTimer;
+
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase;
+import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR;
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+import com.neuronrobotics.sdk.util.ThreadUtil;
+import com.neuronrobotics.sdk.addons.kinematics.IDriveEngine;
+
+
+double stepOverHeight=5;
+long stepOverTime=100;
+Double zLock=-15;
+Closure calcHome = { DHParameterKinematics leg -> 
+		TransformNR h=leg.calcHome() 
+
+		TransformNR tr = leg.forwardOffset(new TransformNR())
+		tr.setZ(zLock)
+		
+		return tr;
+
+}
+boolean usePhysicsToMove = true;
+
+
+return ScriptingEngine.inlineGistScriptRun("bcb4760a449190206170", 
+"GenericWalkingEngine.groovy" , 
+[stepOverHeight,stepOverTime,zLock,calcHome,usePhysicsToMove]
+);
