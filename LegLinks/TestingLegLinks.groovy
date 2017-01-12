f0e3c4a97279f40c3c910945f8f067a2963b8f03
Philip R. Onffroy <datamaniac18@gmail.com>
2017-01-12 09:14:35 -0500

    Added new Rotated Link

--- LegLinks/TestingLegLinks.groovy
+++ LegLinks/TestingLegLinks.groovy
@@ -9,8 +9,10 @@ import eu.mihosoft.vrl.v3d.Transform;
 import com.neuronrobotics.bowlerstudio.physics.TransformFactory;
 
 def remoteLegPiece = ScriptingEngine.gitScriptRun(
-            "https://gist.github.com/6a7ebd3799e086e9b1912c5e7d73125f.git", // git location of the library
-            "DogLegShoulder.groovy" , // file to load
+            /*"https://github.com/DotSlash-CTF/Bulldog.git", // git location of the library
+            "LegLinks/LegMethods.groovy",*/ // file to load
+            "https://gist.github.com/6a7ebd3799e086e9b1912c5e7d73125f.git",
+            "DogLegShoulder.groovy",
             null
             );
 
@@ -20,12 +22,17 @@ CSG servo = com.neuronrobotics.bowlerstudio.vitamins.Vitamins
 CSG horn = com.neuronrobotics.bowlerstudio.vitamins.Vitamins
 .get( "hobbyServoHorn","standardMicro1")
 
-int length = 90
+//shortest is 50, largest is 180
+int length = 80
 ArrayList<CSG> shoulder = remoteLegPiece.createShoulder(servo, length)
 
 ArrayList<CSG> thigh = remoteLegPiece.createThigh(servo, horn, length)
 
+ArrayList<CSG> rotatedLink = remoteLegPiece.rotatedLegLink(servo, horn, length)
+//rotatedLink.get(0) = rotatedLink.get(0).movez(50)
+
 CSG connector = remoteLegPiece.createConnector(servo, horn, length)
+conector = connector.movez(30)
 connector = connector.makeKeepaway(-2)
 					
 for(int i = 0; i < thigh.size(); i++)
@@ -40,15 +47,27 @@ for(int i = 0; i < shoulder.size(); i++)
 								return arg0.toZMin();
 			})
 }
+for(int i = 0; i < rotatedLink.size(); i++)
+{
+	rotatedLink.get(i).setManufactuing({CSG arg0 ->
+								return arg0.toZMin();
+			})
+}
 connector.setManufactuing({CSG arg0 ->
 						return arg0.toZMin();
 			})
+			
 
-ArrayList<CSG> totalParts = thigh;
+
+ArrayList<CSG> totalParts = thigh ;
 totalParts.add(connector)
 for(int i = 0; i < shoulder.size(); i++)
 {
 	//make smaller by nozzle diameter x 2, 0.5*2 makeKeepaway -- createThigh
 	totalParts.add(shoulder.get(i))
+}
+for(int i = 0; i < rotatedLink.size(); i++)
+{
+	totalParts.add(rotatedLink.get(i))
 }
 return totalParts//[horn.movey(100).movex(-20), shoulder, thigh, connector]
