140f9d78a4dcd555ac18b74c5e514d758ad3c45e
myufus <myufus@LuminousShenanigans-ThinkPad-X220>
2017-01-12 21:52:43 -0500

    Added tail appendage

--- Tail/tailDesign.groovy
+++ Tail/tailDesign.groovy
@@ -1,11 +1,13 @@
-CSG bone1 = new Cylinder(5, 5, 10, (int) 30).toCSG().setColor(javafx.scene.paint.Color.WHITE)
+CSG bone1 = new Cylinder(5, 5, 10, (int) 30).toCSG()
 CSG connector1 = new Cylinder(3, 3, 10, (int) 30).toCSG().movez(7.5).setColor(javafx.scene.paint.Color.BLACK)
-CSG bone2 = bone1.movez(15).setColor(javafx.scene.paint.Color.WHITE)
+CSG bone2 = bone1.movez(15)
 CSG connector2 = connector1.movez(15).setColor(javafx.scene.paint.Color.BLACK)
-CSG bone3 = bone2.movez(15).setColor(javafx.scene.paint.Color.WHITE)
+CSG bone3 = bone2.movez(15)
 
-bone1 = bone1.difference(connector1)
-bone2 = bone2.difference(connector1).difference(connector2)
-bone3 = bone3.difference(connector2)
+bone1 = bone1.difference(connector1).setColor(javafx.scene.paint.Color.WHITE)
+bone2 = bone2.difference(connector1).difference(connector2).setColor(javafx.scene.paint.Color.WHITE)
+bone3 = bone3.difference(connector2).setColor(javafx.scene.paint.Color.WHITE)
 
-return[bone1, bone2, bone3, connector1, connector2]
+CSG hornAttachment = new Cylinder(10, 10, 20, (int) 30).toCSG().movez(-20)
+
+return[bone1, bone2, bone3, connector1, connector2, hornAttachment]
