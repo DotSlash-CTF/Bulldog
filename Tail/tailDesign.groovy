1152f1b3756eaa57b69b44ff75120dc7877fcd8c
myufus <myufus@LuminousShenanigans-ThinkPad-X220>
2017-01-12 19:15:34 -0500

    Completed tail aesthetics
    Black connecotrs to be made with flexible material for fun wagging effect

--- Tail/tailDesign.groovy
+++ Tail/tailDesign.groovy
@@ -1,1 +1,6 @@
-CSG tail = new Cylinder(10, 10, 40, (int)30).toCSG()
+CSG bone1 = new Cylinder(5, 5, 10, (int) 360).toCSG().setColor(javafx.scene.paint.Color.WHITE)
+CSG connector1 = new Cylinder(3, 3, 10, (int) 360).toCSG().movez(7.5).setColor(javafx.scene.paint.Color.BLACK)
+CSG bone2 = bone1.movez(15).setColor(javafx.scene.paint.Color.WHITE)
+CSG connector2 = connector1.movez(15).setColor(javafx.scene.paint.Color.BLACK)
+CSG bone3 = bone2.movez(15).setColor(javafx.scene.paint.Color.WHITE)
+return[bone1, bone2, bone3, connector1, connector2]
