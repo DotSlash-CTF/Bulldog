761e7031e7cd4d8b16a79fbcc7755bc448931799
myufus <myufus@LuminousShenanigans-ThinkPad-X220>
2017-01-18 12:41:31 -0500

    Added print statement

--- LegLinks/feetCad.groovy
+++ LegLinks/feetCad.groovy
@@ -39,6 +39,8 @@ class Feet implements ICadGenerator, IParameterChanged{
 		ArrayList<DHLink> dhLinks=d.getChain().getLinks();
 		DHLink dh = dhLinks.get(linkIndex)
 
+		println "feetCad run"
+
 		//The link configuration
 		LinkConfiguration conf = d.getLinkConfiguration(linkIndex);
 		// creating the servo
