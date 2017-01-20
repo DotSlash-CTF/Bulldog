0f9c4264ee4b7631b543d02bd0bf2a3e7a3cc696
Andrew Turley <aturley123@gmail.com>
2017-01-20 09:09:50 -0500

    Fixed formatting

--- Arduino/ServoCode.groovy
+++ Arduino/ServoCode.groovy
@@ -5,18 +5,17 @@ import net.java.games.input.Event;
 import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
 
-BowlerJInputDevice g=null;// Create a variable to store the device
-//Check if the device already exists in the device Manager
-if(DeviceManager.getSpecificDevice(BowlerJInputDevice.class, "jogController")==null){
-	BowlerStudio.speak("I did not find a device named jogController. Select a port to connect to the device.");
+BowlerJInputDevice g = null; //Controller variable
+if (DeviceManager.getSpecificDevice(BowlerJInputDevice.class, "jogController") == null) {
+	System.out.println("I did not find a device named jogController. Select a port to connect to the device.");
 	//If the device does not exist, prompt for the connection
 	g = new BowlerJInputDevice(ControllerEnvironment.getDefaultEnvironment().getControllers()[0]); // This is the DyIO to talk to.
 	g.connect(); // Connect to it.
-	// add the device to the maager
+	//Add the device to the manager
 	DeviceManager.addConnection(g,"jogController");
-}else{
-	//the device is already present on the system, load the one that exists.
-  g=(BowlerJInputDevice)DeviceManager.getSpecificDevice(BowlerJInputDevice.class, "jogController")
+} else {
+	//Load the controller
+  	g = (BowlerJInputDevice)DeviceManager.getSpecificDevice(BowlerJInputDevice.class, "jogController")
 }
 //Set the DyIO into cached mode
 //dyio.setCachedMode(true);
@@ -26,28 +25,26 @@ ServoChannel tilt = new ServoChannel(dyio.getChannel(10))
 
 IJInputEventListener listener = new IJInputEventListener() {
 	@Override public void onEvent(Component comp, Event event1,float value, String eventString) {
-		int val = (int)(93+77*value)
-		try{
-			if(comp.getName().equals("x")){
-				//System.out.println(comp.getName()+" is value= "+value);
+		int val = (int)(93 + 77*value)
+		try {
+			if (comp.getName().equals("x")) {
 				System.out.println(val);
 				pan.SetPosition(val);
 			}
-			if(comp.getName().equals("y")){
-				//System.out.println(comp.getName()+" is value= "+value);
+			if (comp.getName().equals("y")) {
 				tilt.SetPosition(val);
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace(System.out)
 		}
-		System.out.println(comp.getName()+" is value= "+value);
+		System.out.println(comp.getName() + " is value= " + value);
 	}
 }
 g.clearListeners()
 // gamepad is a BowlerJInputDevice
 g.addListeners(listener);
 // wait while the application is not stopped
-while(!Thread.interrupted()){
+while (!Thread.interrupted()) {
 	ThreadUtil.wait(20)
 	//dyio.flush(0)
 }
