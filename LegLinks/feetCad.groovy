0e729a260eb260cadf7e48c6c9c29a9e08d9020f
myufus <myufus@LuminousShenanigans-ThinkPad-X220>
2017-02-28 14:08:01 -0500

    created ball at end of foot

--- LegLinks/feetCad.groovy
+++ LegLinks/feetCad.groovy
@@ -61,7 +61,7 @@ class Feet implements ICadGenerator, IParameterChanged{
           }
           else{
           	piece = remoteLegPiece.createConnector(servoReference.movey(-3), horn, (int) dh.getR() - 30).movex(dh.getR())
-          	piece = piece.union(new Sphere(10).toCSG())
+          	defaultCadGen.add(allCad, (defaultCadGen.moveDHValues(new Sphere(10).toCSG(), dh)).movex(dh.getR()), dh.getListener())
           }//remoteLegPiece.createConnector(servoReference.movey(-3), horn, (int) dh.getR())
           
           //CSG conn = remoteLegPiece.createBaseLink(servoReference.movey(-3), horn, (int) dh.getR(), linkIndex == 0)
