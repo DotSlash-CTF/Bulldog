592c65684356f47d2affed13b81c3d31951cc4f5
Pj Mara <funpjm@gmail.com>
2017-04-03 14:06:12 -0400

    Changed stepovertime to hopefully be more realistic

--- walkingGait.groovy
+++ walkingGait.groovy
@@ -16,7 +16,7 @@ import com.neuronrobotics.sdk.addons.kinematics.IDriveEngine;
 
 
 double stepOverHeight=5;
-long stepOverTime=100;
+long stepOverTime=300;
 Double zLock=0;
 Closure calcHome = { DHParameterKinematics leg -> 
 		TransformNR h=leg.calcHome() 
