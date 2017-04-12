f0f886087c55d2ae88a8021a27cfc59136fc4db2
Andrew Turley <aturley123@gmail.com>
2017-04-12 14:58:06 -0400

    Fixed compiling of neck cad

--- Body/NeckCad.groovy
+++ Body/NeckCad.groovy
@@ -8,7 +8,7 @@ import eu.mihosoft.vrl.v3d.FileUtil;
 class Neck {
 	ArrayList<CSG> makeNeck() {
 		ArrayList<CSG> fullHead = new ArrayList<CSG>()
-		ArrayList<CSG> channel = createBase()
+		CSG channel = generateBody()
 
 		int xSize = 40
 		int ySize = 50
@@ -17,24 +17,28 @@ class Neck {
 		CSG base = new Cube(xSize, ySize, zSize).toCSG()
 		CSG screw = Vitamins.get("capScrew","8#32").makeKeepaway(1.0)
 
-		base = base.movex(188).movez(88.3)
-		base = base.union(new Cube(xSize/2, ySize, zSize/2).toCSG().movex(164.2).movez(78)).setColor(javafx.scene.paint.Color.WHITE)
-		base = base.difference(channel)
+		base = base.movex(188).movez(zSize / 2)
+		base = base.toXMin().movex(channel.getMaxX() - ((base.getMaxX()-base.getMinX())/2)).difference(channel)
 		for (int i = 0; i < 20; i++) {
-			base = base.difference(channel.get(1).movey(i))
+			base = base.difference(channel)
 		}
 
-		base = base.union(screw.roty(180).movex(186).movez(70))
+		base = base.union(screw.roty(180).movex(channel.getMaxX()))
 		
 		fullHead.add(channel)
 		fullHead.add(base)
 		return [fullHead]
 	}
 
-	private ArrayList<CSG> createBase() {
-		ArrayList<CSG> base = ScriptingEngine.gitScriptRun("https://github.com/DotSlash-CTF/Bulldog.git", "FullDog/laserCutCad.groovy", null).generateBody(new MobileBase());//Vitamins.get("vexCchannel", "2x20")
-		
-		return base
+	public CSG generateBody()
+	{
+		CSG crossChannel = centerOnX(Vitamins.get("vexCchannel", "5x20").roty(180).rotz(90))
+	}
+
+	private CSG centerOnX(CSG start)
+	{
+		double yWidth = start.getMaxY() - start.getMinY();
+		return start.toYMin().movey(-(yWidth / 2));
 	}
 }
 ArrayList<CSG> fullHead = new Neck().makeNeck()
