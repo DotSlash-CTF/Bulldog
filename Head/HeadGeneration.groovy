42c93bc8635d5502d9ee4a8c257e3d1cd383d556
CounterfeitLlama <CounterfeitLlama@10.20.1.50>
2017-05-25 12:53:14 -0400

    Fixed nubbin

--- Head/HeadGeneration.groovy
+++ Head/HeadGeneration.groovy
@@ -1206,8 +1206,8 @@ class Headmaker implements IParameterChanged{
 		slot=slot.difference(pin)
 		CSG screw = Vitamins.get("capScrew","8#32").makeKeepaway(1.0).roty(90).scalex(3).movex(-3)
 		CSG nut = Vitamins.get("nut", "8#32").makeKeepaway(1.0).roty(90)
-		nut = nut.union(nut.movex(15)).hull().movex(20)
-		slot = slot.union(screw).union(nut).movex(-4)
+		nut = nut.union(nut.movex(15)).hull().movex(16)
+		slot = slot.union(screw).union(nut).movex(0)
 		for (int i=1;i<4;i++){
 			eye=eye
 			.difference(
